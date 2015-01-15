006729a7a4de821d349481bfa1e6d73a321bc10f
SvenShao <427729776@qq.com>
2015-01-15 08:51:53 +0800

    test code

---
+++ Several-Categories/Several-Categories/Several_Categories.h
@@ -0,0 +1,13 @@
+//
+//  Several_Categories.h
+//  Several-Categories
+//
+//  Created by sven on 15/1/15.
+//  Copyright (c) 2015年 Sven. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Several_Categories : NSObject
+
+@end
---
+++ Several-Categories/Several-Categories/Several_Categories.m
@@ -0,0 +1,13 @@
+//
+//  Several_Categories.m
+//  Several-Categories
+//
+//  Created by sven on 15/1/15.
+//  Copyright (c) 2015年 Sven. All rights reserved.
+//
+
+#import "Several_Categories.h"
+
+@implementation Several_Categories
+
+@end
